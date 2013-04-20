0db3f308352e44ebd4d4a40d4b40e09453caf8fa
Bryan Berger <bryan@bryanberger.com>
2013-04-20 16:39:17 -0400

    Update map to use Google Maps tiles

--- waterscout/js/Map.js
+++ waterscout/js/Map.js
@@ -1,14 +1,12 @@
 // create a map in the "map" div, set the view to a given place and zoom
-var map = L.map('map');//.setView([51.505, -0.09], 13);
 var endpoint = 'http://data.mwater.co/waterscout/apiv2/sources';
+
+var map = L.map('map');
+map.addLayer(new L.Google("SATELLITE"));
 
 // add an OpenStreetMap tile layer
 //L.tileLayer('http://tile.osmosnimki.ru/basesat/{z}/{x}/{y}.jpg', {}).addTo(map);
 //L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {}).addTo(map);
-//map.addLayer(new L.Google());
-L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
-  maxZoom: 18,
-}).addTo(map);
 
 
 // current location
@@ -42,8 +40,10 @@ $.ajax({ url: endpoint}).done(function( data ) {
   	{
 
   		var loc = new L.LatLng(obj.latitude, obj.longitude);
-  		L.marker( loc ).addTo(map);
+      var marker = L.marker( loc ).addTo(map);
 
+      if(obj.desc != '')
+        marker.bindPopup( '<p>'+obj.desc+'</p>' );
   	}
 
   }
